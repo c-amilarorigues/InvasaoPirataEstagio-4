da83c56694fc6e98d9236ece9d73c3f7b4b6064e
c-amilarorigues <81708586+c-amilarorigues@users.noreply.github.com>
2021-06-08 10:06:26 -0300

    Update Boat.js

--- js/Boat.js
+++ js/Boat.js
@@ -15,7 +15,7 @@ class Boat {
     World.add(world, this.body);
   }
 
-//to remove ships from the world
+//para remover os navios do mundo
   remove(index) {
     Matter.World.remove(world, boats[index].body);
     boats.splice(index, 1);
